df61897de4427ddd1926e392119b2fb74ecc5953
Rahul Tiwari <rahultwr0005@gmail.com>
2021-08-23 18:19:13 +0530

    key updated

--- config/service_account_key.js
+++ config/service_account_key.js
@@ -1,10 +1,11 @@
 require("dotenv").config({ path: __dirname + "/.env"});
+console.log(String(process.env.PRIVATE_KEY));
 
 const serviceAccountKey = {
     "type": "service_account",
     "project_id": "theatronfinal",
     "private_key_id": `${process.env.PRIVATE_KEY_ID}`,
-    "private_key": `${process.env.PRIVATE_KEY}`,
+    "private_key": String(process.env.PRIVATE_KEY),
     "client_email": `${process.env.CLIENT_EMAIL}`,
     "client_id": `${process.env.CLIENT_ID}`,
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
